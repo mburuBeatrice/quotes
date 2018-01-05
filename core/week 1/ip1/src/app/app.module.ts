b39b66804c5c11f533850f8caa583109fd2278ff
beatrice mburu <mburubeatricewanjiru@gmail.com>
2018-01-05 15:12:46 +0300

    create a basic skeleton of my project

---
+++ core/week 1/ip1/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
